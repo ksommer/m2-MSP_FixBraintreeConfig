b339968802670cfdf06bd2791a5daa3d9461b8c6
Andrae Gentili <andrea.gentili@magespecialist.it>
2017-12-15 17:08:32 +0100

    Issue 12298 - fix

---
+++ Plugin/Model/Adminhtml/System/Config/CountryCreditCardPlugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrea
+ * Date: 15/12/17
+ * Time: 11.49
+ */
+namespace MSP\Issue12298\Plugin\Model\Adminhtml\System\Config;
+
+class CountryCreditCardPlugin
+{
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    private $serializer;
+
+    /**
+     * @param \Magento\Framework\Serialize\Serializer\Json $serializer
+     */
+    public function __construct(
+        \Magento\Framework\Serialize\Serializer\Json $serializer = null
+    ) {
+        $this->serializer = $serializer ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(\Magento\Framework\Serialize\Serializer\Json::class);
+    }
+
+    public function aroundBeforeSave(\Magento\Braintree\Model\Adminhtml\System\Config\CountryCreditCard $subject, callable $proceed)
+    {
+
+        $value = json_decode(
+            $subject->getValue(\Magento\Braintree\Gateway\Config\Config::KEY_COUNTRY_CREDIT_CARD),
+            true
+        );
+
+        $result = [];
+        if (is_array($value)) {
+            foreach ($value as $data) {
+                if (empty($data['country_id']) || empty($data['cc_types'])) {
+                    continue;
+                }
+                $country = $data['country_id'];
+                if (array_key_exists($country, $result)) {
+                    $result[$country] = $subject->appendUniqueCountries($result[$country], $data['cc_types']);
+                } else {
+                    $result[$country] = $data['cc_types'];
+                }
+            }
+        }
+        $subject->setValue($this->serializer->serialize($result));
+        return $subject;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MSP_Issue12298',
+    __DIR__
+);
